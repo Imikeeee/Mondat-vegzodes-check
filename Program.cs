ce9d89adf12356d60c7bf74bc53df32126b0884a
Imikeeee <38005932+Imikeeee@users.noreply.github.com>
2023-06-13 17:21:51 +0200

    Add files via upload
    Kérjen be egy mondatot, ami ha nem felel meg az alábbiaknak, akkor ismételje meg!
      - Mondat végén legyen a következők valamelyike [ . ! ? ]!
      - A mondat nagybetűvel kezdődjön!
      - A mondat legalább három szóból álljon! Megjegyzés: szóköz a szóelválasztó
    Ha megfelel, akkor legyen vége a mondatnak.
    
    + hibakezeles

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sentence = "";
+        bool isValid = false;
+
+        while (!isValid)
+        {
+            Console.Write("Kérem adjon meg egy mondatot: ");
+            sentence = Console.ReadLine();
+
+            isValid = CheckSentence(sentence);
+            if (!isValid)
+            {
+                Console.WriteLine("A megadott mondat nem felel meg a kritériumoknak. Kérjük próbálja újra.");
+            }
+        }
+
+        Console.WriteLine("A megadott mondat megfelel a feltételeknek.");
+        Console.ReadLine();
+    }
+
+    static bool CheckSentence(string sentence)
+    {
+        if (sentence.Length < 3)
+        {
+            return false;
+        }
+
+        if (!char.IsUpper(sentence[0]))
+        {
+            return false;
+        }
+
+        char lastChar = sentence[sentence.Length - 1];
+        if (lastChar != '.' && lastChar != '!' && lastChar != '?')
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
